3aed74153b00fb8da0cc5a7ebd6813b87b421275
April Sears <aprilnsears@gmail.com>
2025-01-03 11:39:12 -0500

    updated JS file to render response for age

--- js/assignment.js
+++ js/assignment.js
@@ -1,24 +1,23 @@
 "use strict";
 
-// Create your references to the html elements here
-const ageInputEl = document.getElementById("age-input")
+const ageInputEl = document.getElementById("age-input");
 const submissionBtn = document.getElementById("submission-btn");
-
-// create a mutable variable called age and do not assign it a value.
-
+const responseEl = document.getElementById("response");
+let age;
 
 function checkAgeAndRespond() {
     age = parseInt(ageInputEl.value);
     
-    // Write your code below
-
+    if (age >= 21) {
+        responseEl.textContent = "You can vote and purchase alcohol.";
+    } else if (age >= 18) {
+        responseEl.textContent = "You can vote, but you cannot purchase alcohol.";
+    } else {
+        responseEl.textContent = "You cannot vote and you cannot purchase alcohol.";
+    }
 }
 
-
-submissionBtn.addEventListener("click", function () {
-    checkAgeAndRespond();
-});
-
+submissionBtn.addEventListener("click", checkAgeAndRespond);
 
 function validateInput(event) {
     const invalidCharacters = ["e", "E", "-", "+"];
