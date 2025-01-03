98a959a11f117094f0b87b678a161eed943fe52c
April Sears <aprilnsears@gmail.com>
2025-01-03 11:50:05 -0500

    Created examples in index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+// example of if statement
+let nineLives = false;
 
-let healthPoints = 8;
+if (nineLives === true) {
+    console.log("You have more life");
+}
+
+// example of if/else statement
+
+let danIsADog = false;
+
+if (danIsADog === true) {
+    console.log("WOOF");
+} else {
+    console.log("Dan is a human.");
+}
+
+//example of if/else if/else
+
+let healthPoints = 7;
 
 if (healthPoints >= 10) {
     console.log("Hero is healthy.");
